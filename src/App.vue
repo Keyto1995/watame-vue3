6772535770c7856b38f1eb6cb9e8b84293777a5b
keyto1995 <keyto1995@outlook.com>
2020-10-20 14:14:51 +0800

    简洁代码;

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">{{ msg }}</router-link>
   </div>
   <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-export default defineComponent({});
+export default defineComponent({
+  data() {
+    return {
+      msg: "POI",
+    };
+  },
+});
 </script>
 
 <style lang="scss"></style>
